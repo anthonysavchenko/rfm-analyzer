7d294cf739962409937d61d6b4db2b5425f84fc0
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-04-15 11:20:25 +1000

    Cleaned up

--- rfm_analyser/settings/production.py
+++ rfm_analyser/settings/production.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2jirkffja=748-x6yugx8&n0)3929$_z++cz587+r=$ob1zn('
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
