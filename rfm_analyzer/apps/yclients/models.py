03f05d12078df60beb1ceb7bc7e5995d6cc41828
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-05-05 13:11:00 +1000

    last_update field is not required on the form

--- rfm_analyzer/apps/yclients/models.py
+++ rfm_analyzer/apps/yclients/models.py
@@ -19,7 +19,7 @@ class Config(models.Model):
     user_token = models.CharField(max_length=255)
     # All data from data providers (YClients) is stored with local dates.
     # But last_update field is stored with UTC, as it recomended.
-    last_update = models.DateTimeField(null=True)
+    last_update = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return str(self.company_id)
