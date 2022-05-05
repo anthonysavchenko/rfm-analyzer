577b8f83fa0aa1a6e9eddb430fc5057f6e406442
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-05-05 13:57:13 +1000

    Different forms for adding and updating user configuration

--- rfm_analyzer/apps/yclients/admin.py
+++ rfm_analyzer/apps/yclients/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from rfm_analyzer.apps.yclients.forms import ConfigAdminForm
+from rfm_analyzer.apps.yclients.forms import AddConfigAdminForm, ChangeConfigAdminForm
 from rfm_analyzer.apps.yclients.models import Config
 
 
@@ -9,10 +9,17 @@ class ConfigAdmin(admin.ModelAdmin):
     """ YClients connection configuration """
 
     list_display = ['user_name', 'user_email', 'company_id', 'last_update']
-    form = ConfigAdminForm
 
     def user_name(self, config):
         return config.user.username
 
     def user_email(self, config):
         return config.user.email
+
+    def add_view(self, request, form_url='', extra_context=None):
+        self.form = AddConfigAdminForm
+        return super(ConfigAdmin, self).add_view(request, form_url, extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.form = ChangeConfigAdminForm
+        return super(ConfigAdmin, self).change_view(request, object_id, form_url, extra_context)
--- rfm_analyzer/apps/yclients/forms.py
+++ rfm_analyzer/apps/yclients/forms.py
@@ -3,11 +3,17 @@ from django.forms import ModelForm, PasswordInput
 from .models import Config
 
 
-class ConfigAdminForm(ModelForm):
+class AddConfigAdminForm(ModelForm):
     class Meta:
         model = Config
-        fields = ('user', 'company_id', 'bearer_token', 'user_token', 'last_update')
+        fields = ('user', 'company_id', 'bearer_token', 'user_token')
         widgets = {
             'bearer_token': PasswordInput(),
             'user_token': PasswordInput(),
         }
+
+
+class ChangeConfigAdminForm(ModelForm):
+    class Meta:
+        model = Config
+        fields = ('user', 'last_update')
